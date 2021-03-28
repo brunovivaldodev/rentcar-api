258502b8238e921ac2236476b7c56b04342c7bcf
Bruno Vivaldo <brunolumeca@live.com>
2021-03-28 16:59:14 +0100

    Docker added

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,6 @@ app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerFile))
 app.use(router)
 
 
-app.listen(3000,()=>{
-    console.log('Server is running at port 3000')
+app.listen(3333,()=>{
+    console.log('Server is running at port 3333')
 })
